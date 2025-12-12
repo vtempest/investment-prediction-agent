1350cc4fa161aa3dbf2ee73995d481a72505b60a
vtempest <you@example.com>
2025-12-12 10:08:02 -0800

    tradingview-scraper

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"T/croNO/sykqvY5NKh3YKfSV0nRD/u6RfmKvzxD85n0=\"\n}"
--- .next/dev/static/chunks/app_page_tsx_6bece898._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_f05371c2._.js",
-  "static/chunks/node_modules_e616d348._.js"
-],
-    source: "dynamic"
-});
